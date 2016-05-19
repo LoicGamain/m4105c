49b4202d71acc7eca21710483a83bd0852960fca
Loïc <loic.gamain1@gmail.com>
2016-05-19 12:42:29 +0200

    ajout menu burger

--- tp2/js/script.js
+++ tp2/js/script.js
@@ -1,12 +1,12 @@
 $('document').ready(function(){
-	$('#menu_burger').click(function()) {
-		if($('#header').is(':visible')) {
-			$('#header').css("display","none");
+	$('#menu_burger').click(function() {
+		if($('#menu').is(':visible')) {
+			$('#menu').css("display","none");
 		}else {
-			$('#header').css("display","block");
-			$('#header').css("float","center");
-			$('#header').css("margin-left","0px");
-			$('#header').css("margin-bottom","0px");
+			$('#menu').css("display","block");
+			$('#menu').css("float","center");
+			$('#menu').css("margin-left","0px");
+			$('#menu').css("margin-bottom","0px");
 		}
 	}
 });
