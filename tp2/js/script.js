e2ccdfa6d9f8ed4d2bc0375653c3f365adce685f
Loïc <loic.gamain1@gmail.com>
2016-05-20 09:18:51 +0200

    ajout menu burger

--- tp2/js/script.js
+++ tp2/js/script.js
@@ -10,6 +10,22 @@ $('document').ready(function(){
 			$('#header').css("margin-bottom","0px");
 			$('#recherche').css("width","35%");
 		}
-	
+
+		// Détection du support
+		if(typeof sessionStorage!='undefined') {
+		  if('message' in sessionStorage) {
+		    alert("Message récupéré");
+		    document.getElementById('prenom').value = sessionStorage.getItem('prenom');
+		    document.getElementById('nom').value = sessionStorage.getItem('nom');
+		    document.getElementById('age').value = sessionStorage.getItem('age');
+		    document.getElementById('ville_naissance').value = sessionStorage.getItem('ville_naissance');
+		    document.getElementById('email').value = sessionStorage.getItem('email');
+		    document.getElementById('photo').value = sessionStorage.getItem('photo');
+		    document.getElementById('message').value = sessionStorage.getItem('message');
+		  }
+		} else {
+		  alert("sessionStorage n'est pas supporté");
+		}
+			
 	});
 });
